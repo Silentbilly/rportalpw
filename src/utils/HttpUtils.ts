d6b4ba5d035d556be4c5e201331512f2b7fef76d
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-09-28 20:58:47 +0300

    HttpUtils refactoring

--- src/utils/HttpUtils.ts
+++ src/utils/HttpUtils.ts
@@ -7,14 +7,16 @@ export class HttpUtils {
   private static async executePost(request: APIRequestContext, address: string, data: object): Promise<APIResponse> {
     logger.info(`Sending POST: ${address}`);
     logger.info(`With parameters:\n ${JSON.stringify(data, null, 2)}`);
+
     const response = await request.post(address, data);
+    
     logger.info(`Response status: ${response.status()} ${response.statusText()}`);
     logger.info(`Response: ${JSON.stringify(await response.text())}`);
     return response;
   }
 
   static async getAuthTokenRequest(request: APIRequestContext): Promise<APIResponse> {
-    return await HttpUtils.executePost(request, `/uat/sso/oauth/token`, {
+    return HttpUtils.executePost(request, `/uat/sso/oauth/token`, {
       data: `grant_type=password&username=${RP_USERNAME}&password=${RP_PASSWORD}`,
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
