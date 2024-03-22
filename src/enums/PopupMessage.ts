59eaa561a31f9458f4010a1608274236c8ab1f2b
Alexander_Kononov1 <alexander_kononov1@epam.com>
2024-03-22 13:54:05 +0300

    deleting files in develop

--- src/enums/PopupMessage.ts
+++
@@ -1,4 +0,0 @@
-export enum PopupMessage {
-  SIGNED_IN_SUCCESSFULLY = "Signed in successfully",
-  YOU_HAVE_BEEN_LOGGED_OUT = "You have been logged out"
-}
--- src/utils/envParameters.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv'; 
-dotenv.config();
-
-export const RP_USERNAME = process.env.RP_USERNAME as string;
-export const RP_PASSWORD = process.env.RP_PASSWORD as string;
-export const BASIC_AUTH_TOKEN = process.env.BASIC_AUTH_TOKEN as string;
