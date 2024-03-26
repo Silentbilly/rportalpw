1011f571212dbc786aadd1c6812a96e07bbf797e
Alexander_Kononov1 <alexander_kononov1@epam.com>
2024-03-26 18:46:10 +0300

    refactored locators names

--- src/business/page-objects/BasePage.ts
+++ src/business/page-objects/BasePage.ts
@@ -2,26 +2,26 @@ import { Locator, Page } from '@playwright/test';
 import { PopupMessage } from '../enums/PopupMessage';
 
 export class BasePage {
-  private readonly USER_AVATAR: Locator;
-  private readonly SUCCESSFULL_LOGIN_MESSAGE: Locator;
-  private readonly SUCCESSFULL_LOGOUT_MESSAGE: Locator;
+  private readonly userAvatar: Locator;
+  private readonly successfullLoginMessage: Locator;
+  private readonly successfullLogoutMessage: Locator;
 
 
   constructor(page: Page) {
-    this.USER_AVATAR = page.getByRole('img', { name: 'avatar' });
-    this.SUCCESSFULL_LOGIN_MESSAGE = page.getByText(PopupMessage.SIGNED_IN_SUCCESSFULLY);
-    this.SUCCESSFULL_LOGOUT_MESSAGE = page.getByText(PopupMessage.YOU_HAVE_BEEN_LOGGED_OUT);
+    this.userAvatar = page.getByRole('img', { name: 'avatar' });
+    this.successfullLoginMessage = page.getByText(PopupMessage.SIGNED_IN_SUCCESSFULLY);
+    this.successfullLogoutMessage = page.getByText(PopupMessage.YOU_HAVE_BEEN_LOGGED_OUT);
   }
 
   async clickOnUserAvatar() : Promise<void> {
-    await this.USER_AVATAR.click();
+    await this.userAvatar.click();
   }
 
   async isLoginSuccessfullMessageAppeared() : Promise<boolean> {
-    return this.SUCCESSFULL_LOGIN_MESSAGE.isVisible();
+    return this.successfullLoginMessage.isVisible();
   }
 
   async isLogoutSuccessfullMessageAppeared() : Promise<boolean> {
-    return this.SUCCESSFULL_LOGOUT_MESSAGE.isVisible();
+    return this.successfullLogoutMessage.isVisible();
   }
 }
